7eb42bbe2f2fc0032be69dd668c6baa46a538b4f
Sean Paget <codeMonkey760@gmail.com>
2022-04-03 15:28:55 -0700

    Wrap app in react navigator framework

--- App.js
+++ App.js
@@ -1,23 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import PodcastList from './Components/PodcastList';
 
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <PodcastList />
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+          <Stack.Navigator>
+              <Stack.Screen name="PodcastsList" component={PodcastList} />
+          </Stack.Navigator>
+          <StatusBar style="auto" />
+      </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
