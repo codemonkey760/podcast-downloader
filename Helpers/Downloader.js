d90e863b0114f754eac61898f8258266a3546d93
Sean P <codeMonkey760@gmail.com>
2022-04-23 17:15:07 -0700

    Start trying to save download to downloads folder

--- Helpers/Downloader.js
+++ Helpers/Downloader.js
@@ -1,4 +1,5 @@
 const FileSystem = require('expo-file-system');
+import * as MediaLibrary from 'expo-media-library'
 const { createUser } = require('../Util/AuthClient');
 const { getPodcastDetails } = require('../Util/PodcastClient');
 
@@ -6,9 +7,18 @@ const credentialsReducer = ({ sessionId, profileId }) => ({ sessionId, profileId
 const streamUrlReducer = response => (response['items'][0]['streamUrl']);
 
 const getFileName = (ext, podcastId) => (podcastId + '.' + ext);
-const getFileExtFromStreamUrl = streamUrl => (streamUrl.split('/',2)[-1].split('?',2)[0].split('.')[-1]);
+const getFileExtFromStreamUrl = streamUrl => (streamUrl.split('/').pop().split('?')[0].split('.').pop());
 
 async function downloadPodcast(podcastId, progressCallback, done, error) {
+    const perm = await MediaLibrary.getPermissionsAsync(true)
+    if (perm.status !== MediaLibrary.PermissionStatus.GRANTED) {
+        console.log('failed to get media library permission ... aborting download')
+
+        done('')
+
+        return
+    }
+
     try {
         console.log('Starting download');
         const credentials = credentialsReducer(await createUser());
@@ -21,16 +31,14 @@ async function downloadPodcast(podcastId, progressCallback, done, error) {
 
         console.log('And here we go');
 
-        const downloader = FileSystem.createDownloadResumable(streamUrl, FileSystem.documentDirectory + fileName, {}, progressCallback);
+        const downloader = FileSystem.createDownloadResumable(streamUrl, FileSystem.direc + fileName, {}, progressCallback);
 
         await downloader.downloadAsync();
+
+        done(fileName);
     } catch (e) {
         error(e);
-
-        return;
     }
-
-    done(fileName);
 }
 
 module.exports = downloadPodcast;
