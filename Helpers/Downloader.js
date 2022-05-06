e68a1a59a7ef46d17bdbcdde18eb04866cffda2b
Sean P <codeMonkey760@gmail.com>
2022-05-06 08:25:14 -0700

    Got a file to move into external storage on my physical device

--- Helpers/Downloader.js
+++ Helpers/Downloader.js
@@ -1,5 +1,6 @@
+// TODO: finish fully replacing expo-file-system
 const FileSystem = require('expo-file-system');
-import * as MediaLibrary from 'expo-media-library'
+import * as RNFS from 'react-native-fs';
 const { createUser } = require('../Util/AuthClient');
 const { getPodcastDetails } = require('../Util/PodcastClient');
 
@@ -27,10 +28,13 @@ async function downloadPodcast(podcastId, progressCallback, done, error) {
         const { uri } = await downloader.downloadAsync();
         console.log('Downloaded to ' + uri);
 
-        const asset = await MediaLibrary.createAssetAsync(uri)
-        console.log(asset)
+        const dest = RNFS.DownloadDirectoryPath + '/' + fileName;
+        console.log('Trying to move');
+        console.log(uri);
+        console.log('to');
+        console.log(dest);
 
-        await MediaLibrary.createAlbumAsync('Podcasts', asset, false)
+        await RNFS.moveFile(uri, RNFS.DownloadDirectoryPath + '/' + fileName);
 
         done(fileName);
     } catch (e) {
