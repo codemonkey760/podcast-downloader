ece5decee8046dec30aba9c943e3dc99202e98c6
Sean P <codeMonkey760@gmail.com>
2022-05-17 18:46:38 -0700

    Remove stray debug statement

--- reducers/podcastListReducer.js
+++ reducers/podcastListReducer.js
@@ -3,8 +3,6 @@ import { REFRESH_PODCAST_LIST } from '../actions/podcastListActions';
 const PODCAST_LIST_INITIAL_STATE = [];
 
 export const podcastListReducer = (state = PODCAST_LIST_INITIAL_STATE, action) => {
-    console.log(JSON.stringify(action))
-
     switch(action.type) {
         case REFRESH_PODCAST_LIST:
             return action.payload.newPodcastList;
