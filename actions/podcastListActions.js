14351d85915c90488bba22dccf347ba2fb4144b9
Sean P <codeMonkey760@gmail.com>
2022-05-17 19:42:05 -0700

    Remove podcast list from redux store

--- actions/podcastListActions.js
+++
@@ -1,8 +0,0 @@
-export const REFRESH_PODCAST_LIST = 'REFRESH_PODCAST_LIST';
-
-export const refreshPodcastList = (newPodcastList) => (
-    {
-        type: REFRESH_PODCAST_LIST,
-        payload: { newPodcastList }
-    }
-);
--- reducers/reducers.js
+++ reducers/reducers.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
 
-import { podcastListReducer } from './podcastListReducer';
 import { selectedProgramReducer } from './programReducer';
 
 export default combineReducers({
     selectedProgramId: selectedProgramReducer,
-    podcastList: podcastListReducer,
 });
