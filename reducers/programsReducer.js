392de0fc467313ad6b241aebb508ff1c0c95ac88
Sean P <codeMonkey760@gmail.com>
2022-05-22 19:14:41 -0700

    Hard code all of my programs

--- reducers/programsReducer.js
+++ reducers/programsReducer.js
@@ -4,6 +4,26 @@ const initialState = {
     20635765: {
         name: "John and Ken Show",
         namingMode: NamingMode.TITLE_REGEX
+    },
+    27202021: {
+        name: "Carl DeMaio Show",
+        namingMode: NamingMode.NONE
+    },
+    30021205: {
+        name: "Red Pilled America",
+        namingMode: NamingMode.NONE
+    },
+    27538440: {
+        name: "Wendy Walsh Show",
+        namingMode: NamingMode.TITLE_REGEX
+    },
+    27401728: {
+        name: "Gary and Shannon Show",
+        namingMode: NamingMode.NONE
+    },
+    20635767: {
+        name: "The Conway Show",
+        namingMode: NamingMode.NONE
     }
 }
 
