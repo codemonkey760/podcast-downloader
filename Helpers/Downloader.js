889dfd074509966c90f2755df96987ae1de037db
Sean P <codeMonkey760@gmail.com>
2022-05-29 20:29:56 -0700

    Move file naming logic into Namer utility

--- Helpers/Downloader.js
+++ Helpers/Downloader.js
@@ -1,28 +1,14 @@
 // TODO: finish fully replacing expo-file-system
 const FileSystem = require('expo-file-system');
 import * as RNFS from 'react-native-fs';
-import delay from '../Util/delay';
 const { createUser } = require('../Util/AuthClient');
 const { getPodcastDetails } = require('../Util/PodcastClient');
+import { getFileName } from '../Util/Namer'
 
 const credentialsReducer = ({ sessionId, profileId }) => ({ sessionId, profileId });
 const streamUrlReducer = response => (response['items'][0]['streamUrl']);
 
-// const getFileName = (ext, podcastId) => (podcastId + '.' + ext);
-const getFileExtFromStreamUrl = streamUrl => (streamUrl.split('/').pop().split('?')[0].split('.').pop());
-
-const getFileName = (ext, podcastId, title) => {
-    const jkTitleRegex = new RegExp("^John & Ken Show Hour (\\d+) \\((\\d+)/(\\d+)\\)$")
-    const fileNamePattern = "jk-$2$3-hr$1"
-
-    if (title.match(jkTitleRegex)) {
-        return title.replace(jkTitleRegex, fileNamePattern) + '.' + ext
-    } else {
-        return 'jk-' + podcastId + '.' + ext
-    }
-}
-
-async function downloadPodcast(podcastId, title, progressCallback, done, error) {
+async function downloadPodcast(program, podcastId, title, progressCallback, done, error) {
     try {
         console.log('Starting download');
         const credentials = credentialsReducer(await createUser());
@@ -30,7 +16,7 @@ async function downloadPodcast(podcastId, title, progressCallback, done, error) 
         const streamUrl = streamUrlReducer(await getPodcastDetails(podcastId, credentials));
         console.log('Got stream url: ' + streamUrl);
 
-        const fileName = getFileName(getFileExtFromStreamUrl(streamUrl), podcastId, title);
+        const fileName = getFileName(program, podcastId, title, streamUrl);
         console.log('Made a filename: ' + fileName);
 
         console.log('And here we go');
