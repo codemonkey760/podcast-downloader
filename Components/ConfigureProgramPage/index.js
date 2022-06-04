d3ca85a8122d6f5862b88e24bb7b9ef36c73c436
Sean Paget <codeMonkey760@gmail.com>
2022-06-04 15:43:22 -0700

    Display program details on the configure program page

--- Components/ConfigureProgramPage/index.js
+++ Components/ConfigureProgramPage/index.js
@@ -4,16 +4,40 @@ import { useSelector } from 'react-redux'
 import { getProgram } from '../../selectors/programs'
 import { View, Text } from 'react-native'
 
+import {
+    Container,
+    Form,
+    DetailHeader,
+    DetailText
+} from './styles'
+
 function ConfigureProgramPage({ route }) {
     const { programId } = route.params
     const program = useSelector(state => getProgram(state, programId))
 
     return (
-        <View>
-            <Text>
-                Program details for: { programId }
-            </Text>
-        </View>
+        <Container>
+            <Form>
+                <DetailHeader>ID</DetailHeader>
+                <DetailText>{programId}</DetailText>
+                <DetailHeader>Name</DetailHeader>
+                <DetailText>{program.name}</DetailText>
+                <DetailHeader>Naming Mode</DetailHeader>
+                <DetailText>{program.namingMode.description}</DetailText>
+                {program.titleRegex && (
+                    <>
+                        <DetailHeader>Title Regex</DetailHeader>
+                        <DetailText>{program.titleRegex}</DetailText>
+                    </>
+                )}
+                {program.fileNamePattern && (
+                    <>
+                        <DetailHeader>File Name Pattern</DetailHeader>
+                        <DetailText>{`${program.fileNamePattern}.{ext}`}</DetailText>
+                    </>
+                )}
+            </Form>
+        </Container>
     )
 }
 
---
+++ Components/ConfigureProgramPage/styles.js
@@ -0,0 +1,30 @@
+import styled from '@emotion/native'
+
+export const Container = styled.View`
+  margin-top: 25px;
+  width: 100%;
+  flex: 1;
+  flex-direction: column;
+  background-color: #595b68;
+`
+
+export const Form = styled.View`
+  margin: 5px;
+  border-radius: 5px;
+  background-color: #bdc8d3;
+  padding-top: 15px;
+  padding-bottom: 15px;
+`
+
+export const DetailHeader = styled.Text`
+  text-align: center;
+  font-size: 16px;
+  font-weight: bold;
+  color: #d32415;
+`
+
+export const DetailText = styled.Text`
+  margin-bottom: 10px;
+  text-align: center;
+  color: #0b0c10;
+`
