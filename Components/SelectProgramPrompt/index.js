e902a59c02274aee4aaf21b249666edd06d31b29
Sean Paget <codeMonkey760@gmail.com>
2022-06-04 14:40:10 -0700

    Spell out the word configure in program instructions

--- Components/SelectProgramPrompt/index.js
+++ Components/SelectProgramPrompt/index.js
@@ -9,7 +9,7 @@ import {
 const SelectProgramPrompt = () => (
     <SelectProgramPromptContainer>
         <SelectProgramPromptHeader>Select a Program</SelectProgramPromptHeader>
-        <SelectProgramPromptText>Touch the left arrow for a program to config it</SelectProgramPromptText>
+        <SelectProgramPromptText>Touch the left arrow for a program to configure it</SelectProgramPromptText>
         <SelectProgramPromptText>Touch the right arrow for a program to download podcasts</SelectProgramPromptText>
     </SelectProgramPromptContainer>
 )
